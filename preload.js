dcdf8e72b6d4aa0628f4e29f2ac89984da3c601a
Aamir <0fromaamir@gmail.com>
2025-10-01 12:49:41 +0530

    Backend ready for deployment

--- preload.js
+++
@@ -1,9 +0,0 @@
-// preload.js
-const { contextBridge } = require("electron");
-const { TextEncoder, TextDecoder } = require("util");
-
-// Expose Node's TextEncoder/TextDecoder safely to renderer
-contextBridge.exposeInMainWorld("util", {
-  TextEncoder,
-  TextDecoder,
-});
